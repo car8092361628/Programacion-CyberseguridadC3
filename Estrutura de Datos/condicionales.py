fb7ae2b7555e816a1b2eb19850895563913e0241
car8092361628 <carlosmiguelgaarcia1991>
2025-10-14 19:42:54 -0400

    Condicionales

---
+++ Estrutura de Datos/condicionales.py
@@ -0,0 +1,10 @@
+#COndicionales
+numero1=int(input("Ingrese un numero: "))
+numero2=int(input("Ingrese otro numero: "))
+
+if numero1>numero2:
+    print(f"El numero {numero1} es mayor que {numero2}")
+else:
+     print(f"El numero {numero2} es mayor que {numero1}")
+        
+        
