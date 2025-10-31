fd5b33d95f44cb2df7dad71290aec95bfca368f1
car8092361628 <carlosmiguelgaarcia1991>
2025-10-30 20:23:49 -0400

    estructura condicionales

---
+++ Estrutura de Datos/ejerciciospython/estructurascondicionale.py
@@ -0,0 +1,8 @@
+#trabajando con condicionales
+Edad = int(input("Por favor, ingresa tu edad: "))
+
+if Edad >= 18:
+    print("Eres mayor de edad.")
+elif Edad < 18 and Edad > 0:
+    print("Eres menor de edad.")
+    
--- Estrutura de Datos/ejerciciospython/prueba.py
+++ Estrutura de Datos/ejerciciospython/prueba.py
@@ -1,2 +1,18 @@
 # Un programa básico de Hola Mundo en Python
 print("¡Hola Mundo!")
+
+#Trabanjando con numero enteros
+edad = 25 # Edad en años
+print("Mi edad es:", edad) # Imprime la edad
+
+#Trabajando con decimales
+pi = 3.1416
+print("El valor de pi es:", pi)
+
+#Trabajando con cadenas de texto
+nombre = "Juan"
+print("Mi nombre es:", nombre)
+
+# Trabajando con booleanos
+es_estudiante = True    
+print("¿Soy estudiante?", es_estudiante)
